a72799e7666e98099898733e62a463a3b434c45b
renyedi <renyedi@ebay.com>
2014-08-20 10:20:37 -0700

    Step 15, fixed corrupted JAR

--- SimpleTodoList/src/com/codepath/apps/simpletodo/TodoActivity.java
+++ SimpleTodoList/src/com/codepath/apps/simpletodo/TodoActivity.java
@@ -1,15 +1,28 @@
 package com.codepath.apps.simpletodo;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TodoActivity extends Activity {
 
+	private ArrayList<String> items;
+	private ArrayAdapter<String> itemsAdapter;
+	private ListView lvItems;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+        
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
     }
 }
